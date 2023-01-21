6c428473560be46dde598174f542b24201932011
Kaio Delphino <github@kaiodelphino.com>
2023-01-21 16:36:33 -0300

    chore: fix test titles
    All test titles was just a copy from get_force_string_test.dart

--- test/get_maybe_string_test.dart
+++ test/get_maybe_string_test.dart
@@ -11,83 +11,56 @@ void main() {
   });
 
   test(
-      'SHOULD return "null" as String '
+      'SHOULD return null '
       'WHEN value is null', () async {
-    expect(
-      Retriever.getMaybeString(kNull, testMapStub),
-      isNull,
-    );
+    expect(Retriever.getMaybeString(kNull, testMapStub), isNull);
   });
 
   test(
-      'SHOULD return "1" as String '
+      'SHOULD return null '
       'WHEN value is 1', () async {
-    expect(
-      Retriever.getMaybeString(kInt, testMapStub),
-      isNull,
-    );
+    expect(Retriever.getMaybeString(kInt, testMapStub), isNull);
   });
 
   test(
-      'SHOULD return "1.0" as String '
+      'SHOULD return null '
       'WHEN value is 1.0', () async {
-    expect(
-      Retriever.getMaybeString(kDouble, testMapStub),
-      isNull,
-    );
+    expect(Retriever.getMaybeString(kDouble, testMapStub), isNull);
   });
 
   test(
-      'SHOULD return "{key: value}" as String '
+      'SHOULD return null '
       'WHEN value is {"key": "value"}', () async {
-    expect(
-      Retriever.getMaybeString(kMap, testMapStub),
-      isNull,
-    );
+    expect(Retriever.getMaybeString(kMap, testMapStub), isNull);
   });
 
   test(
-      'SHOULD return "[1, 2]" as String '
+      'SHOULD return null '
       'WHEN value is [1, 2]', () async {
-    expect(
-      Retriever.getMaybeString(kList, testMapStub),
-      isNull,
-    );
+    expect(Retriever.getMaybeString(kList, testMapStub), isNull);
   });
 
   test(
-      'SHOULD return "true" '
+      'SHOULD return null '
       'WHEN value is true', () async {
-    expect(
-      Retriever.getMaybeString(kBool, testMapStub),
-      isNull,
-    );
+    expect(Retriever.getMaybeString(kBool, testMapStub), isNull);
   });
 
   test(
-      'SHOULD return "{1, 2}" as String '
+      'SHOULD return null '
       'WHEN value is {1, 2}', () async {
-    expect(
-      Retriever.getMaybeString(kSet, testMapStub),
-      isNull,
-    );
+    expect(Retriever.getMaybeString(kSet, testMapStub), isNull);
   });
 
   test(
       'SHOULD return a String '
       'WHEN value is a string Date', () async {
-    expect(
-      Retriever.getMaybeString(kStringDate, testMapStub),
-      isA<String>(),
-    );
+    expect(Retriever.getMaybeString(kStringDate, testMapStub), isA<String>());
   });
 
   test(
-      'SHOULD return "2023-01-01 00:00:00.000" as String '
+      'SHOULD return null '
       'WHEN value is a DateTime(2023)', () async {
-    expect(
-      Retriever.getMaybeString(kDateTime, testMapStub),
-      isNull,
-    );
+    expect(Retriever.getMaybeString(kDateTime, testMapStub), isNull);
   });
 }
